55aa04b770d412d07569c305a23e4a0ef17cab43
Cristinel Pavel Alergus <cristinelpavelalergus@gmail.com>
2024-12-03 00:37:35 +0100

    feat: [DAM2024-ED-0001] Add print message Cristinel Pavel Alergus

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        printMessage();
     }
+
+  private static void printMessage() {
+    System.out.println(
+        "Cristinel Pavel Alergus colaborando con John Michael Posada - UT5 Git - Entornos de desarrollo - DAM 2024");
+  }
 }
